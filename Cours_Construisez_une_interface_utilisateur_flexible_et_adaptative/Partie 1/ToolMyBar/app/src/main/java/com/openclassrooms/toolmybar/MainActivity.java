323c9f3a24829f049a06e18293c854801f6a5b82
Boisney Philippe <phil.boisney@gmail.com>
2017-11-10 16:39:45 +0100

    Adding Toolbar & Handling different actions (Part 1 - End of Chapter 4)

--- Cours_Construisez_une_interface_utilisateur_flexible_et_adaptative/Partie 1/ToolMyBar/app/src/main/java/com/openclassrooms/toolmybar/MainActivity.java
+++ Cours_Construisez_une_interface_utilisateur_flexible_et_adaptative/Partie 1/ToolMyBar/app/src/main/java/com/openclassrooms/toolmybar/MainActivity.java
@@ -2,6 +2,10 @@ package com.openclassrooms.toolmybar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //1 - Configuring Toolbar
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //2 - Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //3 - Handle actions on menu items
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_params:
+                Toast.makeText(this, "Il n'y a rien à paramétrer ici, passez votre chemin...", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_activity_main_search:
+                Toast.makeText(this, "Recherche indisponible, demandez plutôt l'avis de Google, c'est mieux et plus rapide.", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // ----
+
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
     }
 }
