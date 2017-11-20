209e7ebec40a945d19cf5b2d053db67abc23275f
Boisney Philippe <phil.boisney@gmail.com>
2017-11-20 15:39:07 +0100

    Improving Navigation Drawer | MyNavDrawer (Part 3 - End of Chapter 4)

---
+++ Cours_Construisez_une_interface_utilisateur_flexible_et_adaptative/Partie3/MyNavDrawer/app/src/main/java/com/openclassrooms/mynavdrawer/Controllers/Activities/MainActivity.java
@@ -0,0 +1,171 @@
+package com.openclassrooms.mynavdrawer.Controllers.Activities;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.openclassrooms.mynavdrawer.Controllers.Fragments.NewsFragment;
+import com.openclassrooms.mynavdrawer.Controllers.Fragments.ParamsFragment;
+import com.openclassrooms.mynavdrawer.Controllers.Fragments.ProfileFragment;
+import com.openclassrooms.mynavdrawer.R;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //FOR DESIGN
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    //FOR FRAGMENTS
+    private Fragment fragmentNews;
+    private Fragment fragmentProfile;
+    private Fragment fragmentParams;
+
+    //FOR DATAS
+    private static final int FRAGMENT_NEWS = 0;
+    private static final int FRAGMENT_PROFILE = 1;
+    private static final int FRAGMENT_PARAMS = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Configure all views
+        this.configureToolBar();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
+
+        // Show First Fragment
+        this.showFirstFragment();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        // Show fragment after user clicked on a menu item
+        switch (id){
+            case R.id.activity_main_drawer_news :
+                this.showFragment(FRAGMENT_NEWS);
+                break;
+            case R.id.activity_main_drawer_profile:
+                this.showFragment(FRAGMENT_PROFILE);
+                break;
+            case R.id.activity_main_drawer_settings:
+                this.showFragment(FRAGMENT_PARAMS);
+                break;
+            default:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+
+    // Configure Toolbar
+    private void configureToolBar(){
+        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    // Configure Drawer Layout
+    private void configureDrawerLayout(){
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // Configure NavigationView
+    private void configureNavigationView(){
+        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    // ---------------------
+    // FRAGMENTS
+    // ---------------------
+
+    // Show first fragment when activity is created
+    private void showFirstFragment(){
+        Fragment visibleFragment = getSupportFragmentManager().findFragmentById(R.id.activity_main_frame_layout);
+        if (visibleFragment == null){
+            // Show News Fragment
+            this.showFragment(FRAGMENT_NEWS);
+            // Mark as selected the menu item corresponding to NewsFragment
+            this.navigationView.getMenu().getItem(0).setChecked(true);
+        }
+    }
+
+    // Show fragment according an Identifier
+
+    private void showFragment(int fragmentIdentifier){
+        switch (fragmentIdentifier){
+            case FRAGMENT_NEWS :
+                this.showNewsFragment();
+                break;
+            case FRAGMENT_PROFILE:
+                this.showProfileFragment();
+                break;
+            case FRAGMENT_PARAMS:
+                this.showParamsFragment();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // ---
+
+    // Create each fragment page and show it
+
+    private void showNewsFragment(){
+        if (this.fragmentNews == null) this.fragmentNews = NewsFragment.newInstance();
+        this.startTransactionFragment(this.fragmentNews);
+    }
+
+    private void showParamsFragment(){
+        if (this.fragmentParams == null) this.fragmentParams = ParamsFragment.newInstance();
+        this.startTransactionFragment(this.fragmentParams);
+    }
+
+    private void showProfileFragment(){
+        if (this.fragmentProfile == null) this.fragmentProfile = ProfileFragment.newInstance();
+        this.startTransactionFragment(this.fragmentProfile);
+    }
+
+    // ---
+
+    // Generic method that will replace and show a fragment inside the MainActivity Frame Layout
+    private void startTransactionFragment(Fragment fragment){
+        if (!fragment.isVisible()){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.activity_main_frame_layout, fragment).commit();
+        }
+    }
+}
---
+++ Cours_Construisez_une_interface_utilisateur_flexible_et_adaptative/Partie3/MyNavDrawer/app/src/main/java/com/openclassrooms/mynavdrawer/Controllers/Fragments/NewsFragment.java
@@ -0,0 +1,25 @@
+package com.openclassrooms.mynavdrawer.Controllers.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.openclassrooms.mynavdrawer.R;
+
+/**
+ * Created by Philippe on 20/11/2017.
+ */
+
+public class NewsFragment extends Fragment {
+
+    public static NewsFragment newInstance() {
+        return (new NewsFragment());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+}
