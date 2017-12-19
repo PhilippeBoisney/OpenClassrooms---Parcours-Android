0bd8b95ad3b03586ed88ee7bb3bb0a17bb5e8113
Boisney Philippe <phil.boisney@gmail.com>
2017-12-19 13:56:05 +0100

    Creating New App | NetApp (Part 2 - Beginning of Chapter 1)

---
+++ Cours_Récupérez_et_affichez_des_données_distantes/Partie2&3/NetApp/app/src/main/java/com/openclassrooms/netapp/Controllers/Activities/MainActivity.java
@@ -0,0 +1,36 @@
+package com.openclassrooms.netapp.Controllers.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.openclassrooms.netapp.Controllers.Fragments.MainFragment;
+import com.openclassrooms.netapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MainFragment mainFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.configureAndShowMainFragment();
+    }
+
+    // -------------------
+    // CONFIGURATION
+    // -------------------
+
+    private void configureAndShowMainFragment(){
+
+        mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.activity_main_frame_layout);
+
+        if (mainFragment == null) {
+            mainFragment = new MainFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_main_frame_layout, mainFragment)
+                    .commit();
+        }
+    }
+}
---
+++ Cours_Récupérez_et_affichez_des_données_distantes/Partie2&3/NetApp/app/src/main/java/com/openclassrooms/netapp/Controllers/Fragments/MainFragment.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.netapp.Controllers.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.openclassrooms.netapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    // FOR DESIGN
+    @BindView(R.id.fragment_main_textview) TextView textView;
+
+    public MainFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    // -----------------
+    // ACTIONS
+    // -----------------
+
+    @OnClick(R.id.fragment_main_button)
+    public void submit(View view) { }
+
+}
