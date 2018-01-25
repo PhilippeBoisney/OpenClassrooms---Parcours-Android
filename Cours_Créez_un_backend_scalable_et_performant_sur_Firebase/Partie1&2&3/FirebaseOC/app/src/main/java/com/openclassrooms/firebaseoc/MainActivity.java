9c08c437331ce4dbcafe42738bdd013fcf2ab34c
Boisney Philippe <phil.boisney@gmail.com>
2018-01-25 11:59:48 +0100

    Adding Firebase Integration & Email Authentication | FirebaseOC (Part 1 - End of Chapter 3)

--- Cours_Créez_un_backend_scalable_et_performant_sur_Firebase/Partie1&2&3/FirebaseOC/app/src/main/java/com/openclassrooms/firebaseoc/MainActivity.java
+++ Cours_Créez_un_backend_scalable_et_performant_sur_Firebase/Partie1&2&3/FirebaseOC/app/src/main/java/com/openclassrooms/firebaseoc/MainActivity.java
@@ -1,13 +1,93 @@
 package com.openclassrooms.firebaseoc;
 
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.openclassrooms.firebaseoc.base.BaseActivity;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
+    //FOR DESIGN
+    @BindView(R.id.main_activity_coordinator_layout) CoordinatorLayout coordinatorLayout;
+
+    //FOR DATA
+    private static final int RC_SIGN_IN = 123;
+
     @Override
     public int getFragmentLayout() { return R.layout.activity_main; }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    @OnClick(R.id.main_activity_button_login)
+    public void onClickLoginButton() {
+        this.startSignInActivity();
+    }
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    // --------------------
+    // UI
+    // --------------------
+
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    // --------------------
+    // UTILS
+    // --------------------
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
+    }
 
 }
