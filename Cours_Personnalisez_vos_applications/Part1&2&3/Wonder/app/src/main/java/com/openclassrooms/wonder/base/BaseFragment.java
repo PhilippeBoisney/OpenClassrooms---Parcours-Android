e4735133cf0e0aa72fced39317f9ea4b55f275e9
Boisney Philippe <phil.boisney@gmail.com>
2018-04-02 16:24:02 +0200

    Removing wrong imports

--- Cours_Personnalisez_vos_applications/Part1&2&3/Wonder/app/src/main/java/com/openclassrooms/wonder/base/BaseFragment.java
+++ Cours_Personnalisez_vos_applications/Part1&2&3/Wonder/app/src/main/java/com/openclassrooms/wonder/base/BaseFragment.java
@@ -2,7 +2,6 @@ package com.openclassrooms.wonder.base;
 
 import android.os.Bundle;
 import android.support.annotation.VisibleForTesting;
-import android.support.test.espresso.idling.CountingIdlingResource;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
--- Cours_Personnalisez_vos_applications/Part1&2&3/Wonder/app/src/main/java/com/openclassrooms/wonder/controllers/activities/MainActivity.java
+++ Cours_Personnalisez_vos_applications/Part1&2&3/Wonder/app/src/main/java/com/openclassrooms/wonder/controllers/activities/MainActivity.java
@@ -3,7 +3,6 @@ package com.openclassrooms.wonder.controllers.activities;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.BottomNavigationView;
-import android.support.test.espresso.idling.CountingIdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
