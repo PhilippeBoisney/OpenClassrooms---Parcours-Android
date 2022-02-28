05dc968cfb2f0fbe5b5d5c88a1fa840503260aa0
cece93150 <99273917+cece93150@users.noreply.github.com>
2022-02-28 09:28:59 +0100

    Update FakeApiService.java
    modification du code

--- Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -14,8 +14,7 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public List<User> getUsers() {
-        // TODO: A modifier
-        return null;
+        return users;
     }
 
     /**
@@ -24,7 +23,7 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void generateRandomUser() {
-        // TODO: A modifier
+        users.add(User.random());
     }
 
     /**
@@ -32,6 +31,6 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void deleteUser(User user) {
-        // TODO: A modifier
+        users.remove(user);
     }
 }
