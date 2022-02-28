a1b76af9ba539d5793bce0f5f9e20cf978dd2a23
cece93150 <99273917+cece93150@users.noreply.github.com>
2022-02-28 09:26:33 +0100

    Update UserRepository.java
    modification du code

--- Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -7,22 +7,21 @@ import java.util.List;
 
 public class UserRepository {
 
-    private final ApiService apiService; // TODO: A utiliser
+    private final ApiService apiService;
 
     public UserRepository(ApiService apiService) {
         this.apiService = apiService;
     }
 
     public List<User> getUsers() {
-        // TODO: A modifier
-        return null;
+        return apiService.getUsers();
     }
 
     public void generateRandomUser() {
-        // TODO: A modifier
+       apiService.generateRandomUser();
     }
 
     public void deleteUser(User user) {
-        // TODO: A modifier
+        apiService.deleteUser(user);
     }
 }
